3f099c0fb82a45bde5bd236398bbef7220f710fd
syumai <syumai@gmail.com>
2026-09-06 09:43:31 +0900

    Revert "Remove the API reference page"
    This reverts commit 875fb00b325fc3814f99f8897771078cb72b20b7.

--- website/content/reference/meta.ts
+++ website/content/reference/meta.ts
@@ -1,5 +1,5 @@
 export default {
   title: "Reference",
-  pages: ["limits", "licenses", "troubleshooting", "architecture"],
+  pages: ["api", "limits", "licenses", "troubleshooting", "architecture"],
   order: 4,
 };
